6d8f85347bfb20f1a8482b6be5032d5491e62409
imran <imran75hossainkhan@gmail.com>
2022-02-13 00:02:59 +0600

    Project Setup

---
+++ admin/includes/footer.php
@@ -0,0 +1,35 @@
+  <footer class="footer pt-5">
+    <div class="container-fluid">
+      <div class="row align-items-center justify-content-lg-between">
+   
+        <div class="col-lg-12">
+          <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  </main>
+
+
+
+    <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
+
+  </body>
+
+</html>
